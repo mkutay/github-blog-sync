6b0c2bab213cf43367bc8f5ec9884c659ee74968
mkutay <me@mkutay.dev>
2024-07-08 17:58:37 +0300

    it works v1

--- main.ts
+++ main.ts
@@ -1,86 +1,132 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from 'obsidian';
+import * as git from 'isomorphic-git';
+import http from 'isomorphic-git/http/node';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface GithubBlogSyncPluginSettings {
+	githubUsername: string,
+	githubRepoUrl: string,
+	githubPersonalAccessToken: string,
+	gitDirLocation: string,
+	nameOfCommit: string,
+	emailOfCommit: string,
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: GithubBlogSyncPluginSettings = {
+	githubUsername: '',
+	githubRepoUrl: '',
+	githubPersonalAccessToken: '',
+	gitDirLocation: '',
+	nameOfCommit: '',
+	emailOfCommit: '',
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class GithubBlogSyncPlugin extends Plugin {
+	settings: GithubBlogSyncPluginSettings;
+
+	async SyncBlog() {
+		new Notice('Will push to repository');
+
+		const NAME = this.settings.nameOfCommit;
+		const EMAIL = this.settings.emailOfCommit;
+		const PAT = this.settings.githubPersonalAccessToken;
+		const REPO = this.settings.githubRepoUrl;
+		const DIR = this.settings.gitDirLocation;
+
+		const dir = path.join(this.app.vault.adapter.getBasePath(), DIR);
+
+		try {
+			await git.statusMatrix({ fs, dir: path.join(dir, 'public') }).then((status) =>
+				Promise.all(
+					status.map(([filepath, , worktreeStatus]) => {
+						if (filepath.includes('.DS_Store')) {
+							return;
+						}
+						return worktreeStatus ? git.add({ fs, dir, filepath: path.join('public', filepath) }) : git.remove({ fs, dir, filepath: path.join('public', filepath) });
+					})
+				)
+			);
+			await git.statusMatrix({ fs, dir: path.join(dir, 'content') }).then((status) =>
+				Promise.all(
+					status.map(([filepath, , worktreeStatus]) => {
+						if (filepath.includes('.DS_Store')) {
+							return;
+						}
+						return worktreeStatus ? git.add({ fs, dir, filepath: path.join('content', filepath) }) : git.remove({ fs, dir, filepath: path.join('content', filepath) });
+					})
+				)
+			);
+		} catch (e) {
+			new Notice(`Couldn't add updated files. Problem: ${e}`, 10000);
+			return e;
+		}
+
+		const hostname = os.hostname();
+
+		try {
+			await git.commit({
+				fs,
+				dir,
+				message: hostname + ' ' + new Date(),
+				ref: 'main',
+				author: {
+					name: NAME,
+					email: EMAIL,
+				}
+			});
+		} catch(e) {
+			new Notice(`Couldn't commit changes to the branch. Problem: ${e}`, 10000);
+			return e;
+		}
+
+		try {
+			await git.push({
+				fs,
+				http,
+				dir,
+				url: 'https://' + REPO,
+				onAuth: () => ({ username: PAT }),
+			});
+		} catch(e) {
+			new Notice(`Couldn't push changes to the repository. Problem: ${e}`, 10000);
+			return e;
+		}
+
+		new Notice(`GitHub Blog Sync: Pushed to ${REPO}`);
+	}
 
 	async onload() {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('apple', 'Github Blog Sync', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			this.SyncBlog();
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		ribbonIconEl.addClass('github-blog-sync-ribbon');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+			id: 'github-blog-sync-push-to-repo',
+			name: 'Push to repository',
+			callback: () => {
+				this.SyncBlog();
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addSettingTab(new GithubBlogSyncSettingTab(this.app, this));
 	}
 
-	onunload() {
-
-	}
+	onunload() {  }
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -91,44 +137,83 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class GithubBlogSyncSettingTab extends PluginSettingTab {
+	plugin: GithubBlogSyncPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: GithubBlogSyncPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('GitHub Username')
+			.setDesc('')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.githubUsername)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.githubUsername = value;
 					await this.plugin.saveSettings();
 				}));
+
+		new Setting(containerEl)
+			.setName('GitHub Repository Url')
+			.setDesc('In the format github.com/username/repository')
+			.addText(text => text
+				.setPlaceholder('github.com/username/repository')
+				.setValue(this.plugin.settings.githubRepoUrl)
+				.onChange(async (value) => {
+					this.plugin.settings.githubRepoUrl = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('GitHub Personal Access Token')
+			.setDesc('In the format ghp_XXXXXXXXXXXXXXXXXXXXXXXX')
+			.addText(text => text
+				.setPlaceholder('ghp_XXXXXXXXXXXXXXXXXXXXXXXX')
+				.setValue(this.plugin.settings.githubPersonalAccessToken)
+				.onChange(async (value) => {
+					this.plugin.settings.githubPersonalAccessToken = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('GitHub Folder Location')
+			.setDesc('')
+			.addText(text => text
+				.setPlaceholder('/path/to/folder')
+				.setValue(this.plugin.settings.gitDirLocation)
+				.onChange(async (value) => {
+					this.plugin.settings.gitDirLocation = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('GitHub Commit Name')
+			.setDesc('')
+			.addText(text => text
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.nameOfCommit)
+				.onChange(async (value) => {
+					this.plugin.settings.nameOfCommit = value;
+					await this.plugin.saveSettings();
+					}));
+		
+		new Setting(containerEl)
+			.setName('GitHub Commit Email')
+			.setDesc('')
+			.addText(text => text
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.emailOfCommit)
+				.onChange(async (value) => {
+					this.plugin.settings.emailOfCommit = value;
+					await this.plugin.saveSettings();
+					}));
 	}
 }
